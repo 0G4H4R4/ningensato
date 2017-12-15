30afc143c0ff3f1c3bb9791e0292e36ea4f7ecdd
0G4H4R4 <13033@hakodate.kosen-ac.jp>
2017-12-15 10:22:48 +0900

    setting background 1080x810

---
+++ game/player01.java
@@ -0,0 +1,35 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class player01 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class player01 extends Actor
+{
+    /**
+     * Act - do whatever the player01 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        if( Greenfoot.isKeyDown( "right" ) ){
+            setRotation(0);
+            move(4);
+        }
+        else if( Greenfoot.isKeyDown( "left" ) ){
+            setRotation(180);
+            move(4);
+        }
+        else if( Greenfoot.isKeyDown( "up" ) ){
+            setRotation(270);
+            move(4);
+        }
+        else if( Greenfoot.isKeyDown( "down" ) ){
+            setRotation(90);
+            move(4);
+        }
+    }    
+}
